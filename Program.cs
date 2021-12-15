91c62bec979df9b35b9c01adb8cea62e8dee6f74
Arthur Llorens <arthurllorens@hotmail.fr>
2021-12-15 12:06:56 +0100

    inital commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿string Name;
+string Surname;
+int BirthYear;
+Console.WriteLine("Veuillez indiquer votre nom de famille");
+Name = Console.ReadLine();
+Console.WriteLine("Veuillez indiquer votre prénom");
+Surname = Console.ReadLine();
+Console.WriteLine("Veuillez indiquer votre date de naissance");
+BirthYear = Int32.Parse(Console.ReadLine());
+BirthYear = DateTime.Now.Year - BirthYear;
+Console.WriteLine("Appuyez sur entrée pour afficher votre fiche de renseignement");
+Console.ReadLine();
+Console.WriteLine($"Fiche de renseignement\n" +
+    $"* Nom : {Name} \n" +
+    $"* Prénom : {Surname} \n" +
+    $"* Age : {BirthYear} ans");
