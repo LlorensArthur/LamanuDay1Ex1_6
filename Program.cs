0b871adbbcc1e0a8cc535cfefbe2f255fb7be9e1
Arthur Llorens <arthurllorens@hotmail.fr>
2021-12-15 16:29:04 +0100

    Limité la saisie utilisateur de l'année de naissance sur les chiffres inférieurs à l'année actuelle

--- Program.cs
+++ Program.cs
@@ -1,18 +1,30 @@
 ﻿string Name;
 string Surname;
-int BirthYear;
+int BirthYear = 0;
 Console.WriteLine("Veuillez indiquer votre nom de famille");
 Name = Console.ReadLine();
 Console.WriteLine("Veuillez indiquer votre prénom");
 Surname = Console.ReadLine();
 Console.WriteLine("Veuillez indiquer votre date de naissance");
-while(!int.TryParse(Console.ReadLine(), out BirthYear)){
-    Console.WriteLine("Veuillez entrer uniquement un nombre.");
+bool yearIsValid = false;
+while (!yearIsValid)
+{
+    yearIsValid = true;
+    if (!int.TryParse(Console.ReadLine(), out BirthYear))
+    {
+        Console.WriteLine("Veuillez entrer uniquement un nombre.");
+        yearIsValid = false;
+    }
+    if (BirthYear >= DateTime.Now.Year)
+    {
+        Console.WriteLine("Veuillez entrer uniquement une date de naissance valide.");
+        yearIsValid = false;
+    }
 }
 BirthYear = DateTime.Now.Year - BirthYear;
 Console.WriteLine("Appuyez sur entrée pour afficher votre fiche de renseignement");
 Console.ReadLine();
 Console.WriteLine($"Fiche de renseignement\n" +
-    $"* Nom : {Name} \n" +
-    $"* Prénom : {Surname} \n" +
-    $"* Age : {BirthYear} ans");
+$"* Nom : {Name} \n" +
+$"* Prénom : {Surname} \n" +
+$"* Age : {BirthYear} ans");
